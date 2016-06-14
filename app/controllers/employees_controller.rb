dc2a2721777513a5837736f69da9ea1b7c5cf501
JackMarx <seriousfools@gmail.com>
2016-06-14 18:27:55 -0500

    create action

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,4 +1,26 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Unirest.get("http://localhost:3000/api/v1/employees.json").body
+  end
+
+  def new
+    
+  end
+
+  def create
+    @employee = Unirest.post(
+              "http://localhost:3000/api/v1/employees.json",
+              headers: { "Accept" => "application/json" }, 
+              parameters: { 
+                  first_name: params[:first_name],
+                  last_name: params[:last_name],
+                  ssn: params[:ssn],
+                  email: params[:email]
+               }).body
+
+    render :show
+  end
+
   def show
     @employee = Unirest.get("http://localhost:3000/api/v1/employees/#{params[:id]}.json").body
   end
